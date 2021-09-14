4f6f6e8a05611cb3a6f85878599e3c47edf374f2
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-14 12:32:50 -0700

    Fixed translation warnings
    For French and Arabic!

--- app/src/main/java/com/abdul/firstapp/RecyclerActivity.kt
+++ app/src/main/java/com/abdul/firstapp/RecyclerActivity.kt
@@ -16,4 +16,22 @@ class RecyclerActivity : AppCompatActivity() {
         wordsRecyclerView = findViewById(R.id.wordsRecyclerview)
         wordsRecyclerView.layoutManager = LinearLayoutManager(this)
     }
+    /*
+    public Holder(View itemView) {
+        super(itemView);
+        contacts_cardView = (CardView) itemView.findViewById(R.id.contacts_cardView);
+        contacts_Name = (TextView)itemView.findViewById(R.id.contacts_Name);
+        contacts_Tel = (TextView) itemView.findViewById(R.id.contacts_Tel);
+        btnCancel = itemView.findViewById(R.id.btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+
+        });
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            int pos=getAdapterPosition();
+            Toast.makeText(context,"Pick contact :"+datas.get(pos).getName(), Toast.LENGTH_SHORT);
+        });
+    }
+    */
 }
