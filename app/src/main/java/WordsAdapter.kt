b5ef85fea2d4bb3b1f9e327be74e3aa4819beede
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-14 10:50:22 -0700

    Recyclerview

---
+++ app/src/main/java/WordsAdapter.kt
@@ -0,0 +1,30 @@
+package com.abdul.firstapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class WordsAdapter(val words: Array<String>) : RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() {
+
+
+    class WordsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvRowPlank : TextView = itemView.findViewById(R.id.tvRow)
+    }
+
+    //david -- to buy new planks as and when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
+        var plankRow = LayoutInflater.from(parent.context).inflate(R.layout.row_recycler_view,parent,false);
+        return WordsViewHolder(plankRow);
+    }
+
+    //abdullah handwriting-- write the content on the plank
+    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
+        holder.tvRowPlank.setText(words[position])
+    }
+
+    override fun getItemCount(): Int {
+        return words.size
+    }
+}
