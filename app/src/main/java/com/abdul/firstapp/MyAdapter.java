55fa5ba0acc2b8444c33031af98bc160acc6e829
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-15 09:03:25 -0700

    Made background for Async activity!

---
+++ app/src/main/java/com/abdul/firstapp/MyAdapter.java
@@ -0,0 +1,23 @@
+package com.abdul.firstapp;
+
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+*/
