8d5fb381ff117be1e9476a14a84cd0604d0f2998
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-16 09:42:38 -0700

    Connecting to internet part 3 - parsing json

--- app/src/main/java/com/abdul/firstapp/FetchBookTask.java
+++ app/src/main/java/com/abdul/firstapp/FetchBookTask.java
@@ -1,5 +1,7 @@
 package com.abdul.firstapp;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.os.AsyncTask;
 import android.widget.TextView;
 import org.w3c.dom.Text;
@@ -22,5 +24,37 @@ public class FetchBookTask extends AsyncTask<String,Void,String> {
     @Override
     protected void onPostExecute(String jsonString) {
         super.onPostExecute(jsonString);
+
+        try {
+            JSONObject rootJsonObject = new JSONObject(jsonString);
+            JSONArray itemsArray = rootJsonObject.getJSONArray("items");
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title = null;
+                String authors = null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (title != null && authors != null) {
+                    titleTv.setText(title);
+                    authorTv.setText(authors);
+                    return;
+                }
+
+                titleTv.setText("No Results Found");
+                authorTv.setText("");
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
